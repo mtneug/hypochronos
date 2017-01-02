37cf6a40f1651aab4d6e84f5c218d69b3bda1828
Matthias Neugebauer <mtneug@mailbox.org>
2017-01-02 13:08:04 +0100

    Add example json timetable server

---
+++ cmd/hypochronos-json-example/main.go
@@ -0,0 +1,72 @@
+// Copyright (c) 2016 Matthias Neugebauer <mtneug@mailbox.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mtneug/hypochronos/docker"
+	"github.com/mtneug/hypochronos/model"
+	"github.com/mtneug/hypochronos/timetable"
+)
+
+var nodeID string
+
+func init() {
+	info, err := docker.StdClient.Info(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	nodeID = info.Swarm.NodeID
+}
+
+func main() {
+	http.HandleFunc("/tt.json", ttHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ttHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		now  = time.Now().UTC()
+		resp = timetable.JSONFillerResponse{
+			API:      timetable.API{APIVersion: "1"},
+			Metadata: timetable.Metadata{CreatedAt: now},
+			Spec: timetable.JSONFillerSpec{
+				Timetable: map[string]map[time.Time]timetable.State{
+					nodeID: map[time.Time]timetable.State{
+						now.Add(-15 * time.Second): model.StateActivated,
+						now.Add(+15 * time.Second): model.StateDeactivated,
+						now.Add(+45 * time.Second): model.StateActivated,
+						now.Add(+75 * time.Second): model.StateDeactivated,
+					},
+				},
+			},
+		}
+	)
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Print(err)
+		return
+	}
+
+	w.Write(jsonResp)
+	log.Print("GET /tt.json")
+}
